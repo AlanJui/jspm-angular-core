48e7499b9fa7080e6e2ce15ad246fb84306ac558
Alan Jui <alanjui.1960@gmail.com>
2016-03-25 10:27:22 +0800

    Remove client/boot.js

--- client/boot.js
+++
@@ -1,8 +0,0 @@
-import angular from 'angular';
-import 'angular-ui-router';
-
-import main from 'modules/main/main.module';
-
-angular.element(document).ready(function () {
-  return angular.bootstrap(document, [main.name]);
-});
